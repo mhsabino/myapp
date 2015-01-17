8b74db96e06e83a54f771ab93337504e70633359
Mateus Sabino <mateussabino87@gmail.com>
2015-01-17 21:15:18 -0200

    index

---
+++ app/assets/javascripts/flowers.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$("div.col-sm-4 a").fancybox({
+		'hideOnContentClick': false,
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+
+});
+
---
+++ app/controllers/flowers_controller.rb
@@ -0,0 +1,4 @@
+class FlowersController < ApplicationController
+  def index
+  end
+end
